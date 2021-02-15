26f60f08033671a660f0dd02840d68c007f7f420
Foxxite <foxxite@thefoxes.eu>
2021-02-15 14:56:24 +0100

    Solution

---
+++ Cinema App/Cinema App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cinema_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
