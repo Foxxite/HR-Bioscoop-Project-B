81682a1061cd00cad0f90e47606b5f49964c8e2e
Foxxite <foxxite@thefoxes.eu>
2021-03-16 12:54:48 +0100

    Base class files

---
+++ Cinema App/Cinema App/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Cinema_App
+{
+    class Program
+    {
+        private static Controller Controller;
+
+        static void Main(string[] args)
+        {
+            // ToDo Setup Main menu
+            Controller = new Controller(args);
+        }
+    }
+}
