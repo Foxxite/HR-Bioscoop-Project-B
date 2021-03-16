c1bbc16d1bdbd9f53a43947578be3b8e4cd72448
Yannick01 <yannickvreede@gmail.com>
2021-03-16 19:29:48 +0100

    user class attributes

--- Cinema App/Cinema App/User.cs
+++ Cinema App/Cinema App/User.cs
@@ -1,6 +1,45 @@
-﻿namespace Cinema_App
+﻿using System;
+using System.Text;
+
+namespace Cinema_App
 {
-    internal class User
+    class User
     {
+        private Guid GUID;
+
+        private string Username;
+        private string Password;
+
+        private int Permlevel;
+
+        private string Name;
+        private string Address;1
+        private int Age;
+
+
+        private byte[] CreateSalt(string enterPassword)
+        { 
+            var buffer = Encoding.UTF8.GetBytes(Age + Name + enterPassword + Address + Username);
+            for(int i = 0; i < buffer.Length; i++)
+            {
+                buffer[i] += 0b1011001;
+
+
+            }
+            return buffer;
+        }
+
+
+        public bool VerifyPassword(string password)
+        {
+            
+
+
+            return true;
+        }
+
+
+       
+
     }
 }
