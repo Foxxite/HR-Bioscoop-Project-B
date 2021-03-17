0691b69d42d2423e93d88933a15ea5086626f7a1
yannickdv01 <75087107+yannickdv01@users.noreply.github.com>
2021-03-17 14:38:45 +0100

    User (#3)
    * user class attributes
    
    * password abstraction
    
    * documentation added for: user and passwordmanager.

---
+++ Cinema App/Cinema App/PasswordManager.cs
@@ -0,0 +1,59 @@
+﻿using Konscious.Security.Cryptography;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cinema_App
+{
+    /// <summary>
+    /// Handles the creation and verification of passwords using the Argon2id-algorithm. 
+    /// </summary>
+    static class PasswordManager
+    {
+        /// <summary>
+        /// Creates a salt based on the inputted string.
+        /// </summary>
+        /// <param name="salt">String to use as salt.</param>
+        /// <returns>Salt represented as byte-array.</returns>
+        static public byte[] CreateSalt(string salt = "")
+        {
+            var buffer = Encoding.UTF8.GetBytes(salt);
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                buffer[i] += 0b1011001;
+            }
+            return buffer;
+        }
+
+        /// <summary>
+        /// Creates a password hash based on the inputted string and salt.
+        /// </summary>
+        /// <param name="enteredPassword">Password string.</param>
+        /// <param name="salt">Salt represented as byte-array.</param>
+        /// <returns>Argon2 hash encoded in Base64.</returns>
+        static public string HashPassword(string enteredPassword, byte[] salt)
+        {
+            var argon2 = new Argon2id(Encoding.UTF8.GetBytes(enteredPassword));
+
+            argon2.Salt = salt;
+            argon2.DegreeOfParallelism = 8; // four cores
+            argon2.Iterations = 4;
+            argon2.MemorySize = 1024 * 1024; // 1 GB
+
+            return Convert.ToBase64String(argon2.GetBytes(16));
+        }
+
+        /// <summary>
+        /// Verifies password against a Base64 encoded Argon2 hash.
+        /// </summary>
+        /// <param name="enteredPassword">Password to verify.</param>
+        /// <param name="hash">Argon2 hash.</param>
+        /// <param name="salt">Salt.</param>
+        /// <returns>True if match, else False.</returns>
+        static public bool VerifyPassword(string enteredPassword, string hash, byte[] salt)
+        {
+            var newHash = HashPassword(enteredPassword, salt);
+            return hash.Equals(newHash);
+        }
+    }
+}
--- Cinema App/Cinema App/User.cs
+++ Cinema App/Cinema App/User.cs
@@ -1,6 +1,59 @@
-﻿namespace Cinema_App
+﻿using Konscious.Security.Cryptography;
+using System;
+using System.Text;
+
+namespace Cinema_App
 {
-    internal class User
+    /// <summary>
+    /// Class representing a user.
+    /// </summary>
+    
+    class User
     {
+        private Guid GUID;
+
+        private string Username;
+        private string Password;
+
+        private int Permlevel;
+
+        private string Name;
+        private string Address;
+        private int Age;
+
+        /// <summary>
+        /// Creates salt based on user info.
+        /// </summary>
+        /// <param name="enterPassword">String password.</param>
+        /// <returns>Salt based on user info.</returns>
+        private byte[] CreateSalt(string enterPassword)
+        {
+            return PasswordManager.CreateSalt(Age + Name + enterPassword + Address + Username);
+        }
+
+        /// <summary>
+        /// Creates Argon2-hash of password.
+        /// </summary>
+        /// <param name="enteredPassword">String password.</param>
+        /// <returns>Hash.</returns>
+        public string HashPassword (string enteredPassword)
+        {
+            return PasswordManager.HashPassword(enteredPassword, CreateSalt(enteredPassword));
+        }
+
+        /// <summary>
+        /// Verifies password given compared to users password.
+        /// </summary>
+        /// <param name="enteredPassword">Password to verify.</param>
+        /// <returns>True if match, else False.</returns>
+        public bool VerifyPassword(string enteredPassword)
+        {
+            return PasswordManager.VerifyPassword(enteredPassword, Password, CreateSalt(enteredPassword));
+        }
+
+
+
+
+
     }
 }
