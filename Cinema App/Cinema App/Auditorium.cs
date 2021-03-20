a72d6a6bbadd050d70e9a142f12a727f79a489c8
julian <julian-_-vink@hotmail.com>
2021-03-20 15:57:32 +0100

    Added auditorium class

---
+++ Cinema App/Cinema App/Auditorium.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cinema_App
+{
+    /// <summary>
+    ///     Class representing an auditorium.
+    /// </summary>
+    /// <param name = "Name">Name of the auditorium.</param>
+    /// <param name = "Seat">Number of the seat.</param>
+    class Auditorium
+    {
+        private string Name;
+        private int[] Seat;
+    }
+}
