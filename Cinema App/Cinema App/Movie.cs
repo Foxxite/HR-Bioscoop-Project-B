c89bb31123ebd2efe51370f9467371bc1426d6ba
Chakir <1008730@hr.nl>
2021-03-20 14:57:55 +0100

    Added Auditorium class + made movie class

---
+++ Cinema App/Cinema App/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cinema_App
+{
+    class Movie : Item
+    {
+        private string Description;
+        private int AgeRating;
+        private int ReviewScore;
+        private string TrailerUrl;
+        private Auditorium Auditorium;
+
+        public override bool IsAvailable()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
