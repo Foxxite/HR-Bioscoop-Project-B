00812a94c46edcc490b9d709ca75927e0eee6384
Chakir <1008730@hr.nl>
2021-03-22 10:29:40 +0100

    Added documentation to Item and Movie class

--- Cinema App/Cinema App/Item.cs
+++ Cinema App/Cinema App/Item.cs
@@ -5,11 +5,26 @@ using System.Text;
 namespace Cinema_App
 {
     abstract class Item
+
+        ///<summary>
+        ///makes a new class called Item
+        ///</summary>
     {
         protected string Name;
         protected double Price;
         protected int StockAvailable;
 
+        /// <summary>
+        /// Name = gives the name of the movie as a string
+        /// Price = gives the price of the movie as a double
+        /// StockAvailable = shows what stock is available as a integer
+        /// </summary>
+        /// <returns></returns>
+
         public abstract bool IsAvailable();
+
+        ///<summary>
+        ///method to check if seats are available
+        ///</summary>
     }
 }
--- Cinema App/Cinema App/Movie.cs
+++ Cinema App/Cinema App/Movie.cs
@@ -5,6 +5,10 @@ using System.Text;
 namespace Cinema_App
 {
     class Movie : Item
+
+        /// <summary> 
+        /// inheritates the item class
+        /// </summary>
     {
         private string Description;
         private int AgeRating;
@@ -12,9 +16,21 @@ namespace Cinema_App
         private string TrailerUrl;
         private Auditorium Auditorium;
 
+        /// <summary>
+        /// Description = shows a description of the movie as a string
+        /// AgeRating = shows what the minimum age of a movie is as a integer
+        /// ReviewScore = the score of the movie based on the reviews given by people as a integer
+        /// TrailerUrl = gives a link to the trailer of the movie as a string
+        /// Auditorium = shows how many seats are left in the auditorium to reserve by the people
+        /// </summary>
+        /// <returns></returns>
         public override bool IsAvailable()
         {
             throw new NotImplementedException();
+
+            ///<summary>
+            ///methods to check if seats are available
+            ///</summary>
         }
     }
 }
