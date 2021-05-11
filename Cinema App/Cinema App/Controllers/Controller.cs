a7c620df0ecd3705c5f017d1a4230a68e0c9f38a
Foxxite <foxxite@thefoxes.eu>
2021-05-11 15:22:55 +0200

    Basket Class

--- Cinema App/Cinema App/Controllers/Controller.cs
+++ Cinema App/Cinema App/Controllers/Controller.cs
@@ -38,23 +38,24 @@ namespace Cinema_App
             Menu mainMenu = new Menu(this, Strings.MainMenuName);
 
             // Show options if user isn't logged in.
-            if(CurrentUser == null)
+            if (CurrentUser == null)
             {
-                mainMenu.AddMenuOption(Strings.RegisterNew, (x) => { ShowRegistationScreen(); }, false);
-                mainMenu.AddMenuOption(Strings.LoginMenu, (x) => { ShowLoginScreen(); }, false);
+                mainMenu.AddMenuOption(Strings.RegisterNew, (x) => { ShowRegistationScreen(); }, null);
+                mainMenu.AddMenuOption(Strings.LoginMenu, (x) => { ShowLoginScreen(); }, null);
             }
             // Show options if user is logged in.
             else
             {
-                mainMenu.AddMenuOption(Strings.ViewCurrentMovies, (x) => { ViewMovies(); }, false);
-                mainMenu.AddMenuOption(Strings.ViewAcc, (x) => { ViewUserInfo(); }, false);
-                mainMenu.AddMenuOption(Strings.ChangeAcc, (x) => { ChangeUserInfo(); }, false);
-                mainMenu.AddMenuOption(Strings.LogOut, (x) => { LogOut(); }, false);
+                mainMenu.AddMenuOption(Strings.ViewCurrentMovies, (x) => { ViewMovies(); }, null);
+                mainMenu.AddMenuOption("Basket", (x) => { ViewBasket(); }, null); 
+                mainMenu.AddMenuOption(Strings.ViewAcc, (x) => { ViewUserInfo(); }, null);
+                mainMenu.AddMenuOption(Strings.ChangeAcc, (x) => { ChangeUserInfo(); }, null);
+                mainMenu.AddMenuOption(Strings.LogOut, (x) => { LogOut(); }, null);
             }
             
             //Always show those options
-            mainMenu.AddMenuOption(Strings.About, (x) => { About(); }, false);
-            mainMenu.AddMenuOption(Strings.Exit, (x) => { CloseApp(); }, false);
+            mainMenu.AddMenuOption(Strings.About, (x) => { About(); }, null);
+            mainMenu.AddMenuOption(Strings.Exit, (x) => { CloseApp(); }, null);
 
             SwitchView(mainMenu);
         }
@@ -100,6 +101,12 @@ namespace Cinema_App
         {
             View_MovieCatalogue mc = new View_MovieCatalogue(this, Strings.MovCat);
             SwitchView(mc);
+        }
+
+        private void ViewBasket()
+        {
+            View_Basket vb = new View_Basket(this, "Basket");
+            SwitchView(vb);
         }
 
         private void ViewUserInfo()
--- Cinema App/Cinema App/Views/View_Basket.cs
+++ Cinema App/Cinema App/Views/View_Basket.cs
@@ -6,9 +6,12 @@ namespace Cinema_App
 {
     class View_Basket : View
     {
+        Basket Basket;
+
         public View_Basket(Controller controller, string title, string subTitle = "", int permLevel = 0)
            : base(controller, title, subTitle, permLevel)
         {
+            Basket = Controller.Basket;
             return;
         }
 
@@ -16,10 +19,18 @@ namespace Cinema_App
         {
             DrawTitleBar();
 
-            List<BasketItem> basketItems = Controller.Basket.GetAllItems();
+            List<BasketItem> basketItems = Basket.GetAllItems();
 
+            Menu basketMenu = new Menu(Controller, "Basket", $"Total: €{String.Format("{0:N}", Basket.TotalPrice())}");
 
+            foreach (BasketItem item in Basket.GetAllItems())
+            {
+                basketMenu.AddMenuOption(item.Item.Name + " " + item.Quantity + "x", null, null);
+            }
 
+            basketMenu.AddMenuOption(Strings.ReturnToMainOption, (x) => { Controller.ShowMainMenu(); }, null);
+
+            Controller.SwitchView(basketMenu);
         }
     }
 }
