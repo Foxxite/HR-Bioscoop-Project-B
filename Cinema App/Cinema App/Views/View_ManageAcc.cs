31013b776204c188533310773d48cd2c86a34846
Yannick01 <yannickvreede@gmail.com>
2021-06-03 14:59:08 +0200

    rest van admin stuff

--- Cinema App/Cinema App/Views/View_ManageAcc.cs
+++ Cinema App/Cinema App/Views/View_ManageAcc.cs
@@ -2,9 +2,30 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Cinema_App.Views
+namespace Cinema_App
 {
-    class Class1
+    class View_ManageAcc : View
     {
+        public View_ManageAcc(Controller controller, string title, string subTitle = "", int permLevel = 0) : base(controller, title, subTitle, permLevel)
+        {
+
+        }
+
+        public override void Render()
+        {
+            DrawTitleBar();
+
+            Menu menu = new Menu(Controller, "Manage Accounts");
+
+            List<User> Users = Controller.DataStore.GetUsers();
+
+            foreach(User user in Users)
+            {
+                menu.AddMenuOption(user.Username, (x) => { Controller.SwitchView(new View_UserInformation(Controller, "Manage Account", cUser: user)); },null);
+            }
+
+            menu.AddMenuOption(Strings.ReturnToMainOption, (x) => { Controller.ShowMainMenu(); }, null);
+            Controller.SwitchView(menu);
+        }
     }
 }
--- Cinema App/Cinema App/Views/View_UserInformation.cs
+++ Cinema App/Cinema App/Views/View_UserInformation.cs
@@ -6,8 +6,10 @@ namespace Cinema_App
 {
     class View_UserInformation : View
     {
-        public View_UserInformation(Controller controller, string title, string subTitle = "", int permLevel = 0) : base(controller, title, subTitle, permLevel)
+        User CurrentUser;
+        public View_UserInformation(Controller controller, string title, string subTitle = "", int permLevel = 0, User cUser = null) : base(controller, title, subTitle, permLevel)
         {
+            CurrentUser = cUser;
             return;
         }
 
@@ -15,17 +17,39 @@ namespace Cinema_App
         {
             DrawTitleBar();
 
-            DrawField(Strings.VarUserName, Controller.CurrentUser.Username);
-            DrawField(Strings.VarName, Controller.CurrentUser.Name);
-            DrawField(Strings.VarAge, Controller.CurrentUser.Age.ToString());
-            DrawField(Strings.VarEmailAddress, Controller.CurrentUser.EmailAddress);
-
+            if (CurrentUser == null)
+            {
+                DrawField(Strings.VarUserName, Controller.CurrentUser.Username);
+                DrawField(Strings.VarName, Controller.CurrentUser.Name);
+                DrawField(Strings.VarAge, Controller.CurrentUser.Age.ToString());
+                DrawField(Strings.VarEmailAddress, Controller.CurrentUser.EmailAddress);
+            }
+            else
+            {
+                DrawField(Strings.VarUserName, CurrentUser.Username);
+                DrawField(Strings.VarName, CurrentUser.Name);
+                DrawField(Strings.VarAge, CurrentUser.Age.ToString());
+                DrawField(Strings.VarEmailAddress, CurrentUser.EmailAddress);
+            }
             Console.WriteLine();
+
+            if (Controller.CurrentUser.Permlevel == Program.ADMIN_PERM_LEVEL)
+            {
+                View LastView = new View_ManageAcc(Controller, "Manage Accounts");
+
+                Menu menu = new Menu(Controller, "", fullScreen: false);
+                menu.AddMenuOption("Delete", (x) => { Controller.DataStore.DeleteUserData(CurrentUser); Controller.SwitchView(LastView); }, null);
+                menu.AddMenuOption(Strings.ReturnToMainOption, (x) => { Controller.SwitchView(LastView); }, null);
+
+                Controller.SwitchView(menu, false);
+            }
 
             Console.ForegroundColor = ConsoleColor.Gray;
             Console.WriteLine(Strings.KeyPressToReturn);
             Console.ReadKey();
             Controller.ShowMainMenu();
+
+            
         }
 
         /// <summary>
